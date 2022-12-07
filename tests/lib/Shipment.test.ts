751d1adeba1dc8d5b622dad301f6a6a6b6148554
Patrick Organ <patrick@permafrost.dev>
2022-12-06 19:56:03 -0500

    update tests

--- tests/lib/Shipment.test.ts
+++ tests/lib/Shipment.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable array-element-newline */
 import { Driver } from '@/lib/Driver';
 import { Shipment } from '@/lib/Shipment';
 
@@ -11,7 +12,15 @@ it('calculates the correct suitability score for a shipment and driver', () => {
 it('finds the best driver for a shipment', () => {
     const shipment = new Shipment('9 Olive Avenue');
 
-    const drivers = [new Driver('Zachariah Hardwick'), new Driver('Malcolm Parker'), new Driver('Lillian Mccormick')];
+    const drivers = [ new Driver('Zachariah Hardwick'), new Driver('Malcolm Parker'), new Driver('Lillian Mccormick') ];
 
     expect(shipment.getBestDriver(drivers).name).toEqual(drivers[1].name);
+});
+
+it('throws an error when no drivers are provided', () => {
+    const shipment = new Shipment('9 Olive Avenue');
+
+    expect(() => {
+        shipment.getBestDriver([]);
+    }).toThrowErrorMatchingSnapshot();
 });
